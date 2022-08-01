c0e372424793d1f1dd58f3481e158d3e981f741b
Nursultan24 <Nursultanmustafaev15@gmail.com>
2022-08-02 00:15:41 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+// let a = '34';
+// console.log(+a);
+// console.log(Number(a));
+// console.log(a * 1);
+
+
+// let b = 29.92;
+// console.log(Math.floor(b));//округление в меньшую сторону
+// console.log(Math.ceil(b));//округление в большую сторону
+// console.log(Math.round(b));//округление до ближайшего целого сторону
+// console.log(Math.trunc(b));//игнорирует все число после точки
+//
+// let c = 'aBRAcADBRA';
+// console.log(c.toLowerCase());
+// console.log(c.toUpperCase());
+
+// let d  = 'Hello world';
+// console.log(d[0]);
+// console.log(d[1]);
+// console.log(d[d.length-2]);
+// console.log(d[d.length-1]);
+//
+
+// let e = 'Hello world';
+// console.log(e.length);//выводит длину данной строки.
+//
+
+
+
+// let f = 'abracaDabra';
+// console.log(f.includes('f'));//поиск подстроки
+// console.log(f.includes('abracaDabra'));
+//
+// let g = 'Propagination';
+// console.log(g.indexOf('p'));// получения подстроки в обратном направлении
+// console.log(g.lastIndexOf('g'));// получения подстроки
+
+
+
+
+
+
+
