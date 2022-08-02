138646af7cdd2aec7350023da6bfc603b263d1b2
Nursultan24 <Nursultanmustafaev15@gmail.com>
2022-08-02 11:49:29 +0600

    push 3hw

---
+++ 3hw/main.js
@@ -0,0 +1,78 @@
+
+ //keys: values
+ //
+ // let salaries = {
+ //     John: 100,
+ //     Ann: 160,
+ //     Pete: 130
+ // };
+ //
+ // let sum = 0;
+ // for (let key in salaries) {
+ //     sum += salaries[key];
+ // }
+ // console.log(sum);
+
+ // let obj = {
+ //    name: 'Ivan',
+ //     age: 26
+ // };
+ //
+ // console.log(obj.name);
+
+ // let obj = {
+ //    name: 'Ivan',
+ //     age: 26,
+ //     student: true
+ // };
+ // console.log(obj.student='False');
+
+ // let obj = {
+ //     name:'ABRACADABRA',
+ //     age: 15,
+ //     student:true
+ // };
+ //
+ // console.log(obj.name.includes('BRAC');
+
+//  let obj = {name:'Ivan',
+//      age: 26,
+//      student: true};
+//
+// let obj2 = {...obj};
+// obj2.name ='Nurs';
+//
+//  console.log(obj);
+//  console.log(obj2);
+
+
+// let obj = {
+//     name: 'ivan',
+//     age:26
+// };
+//  console.log(obj.age = '31');
+//
+// let arr = ['bmw', 'honda','mers','lexus'];
+//
+//  console.log(arr.length);
+//
+//  let arr = ['bmw', 'honda','mers','lexus'];
+//
+//  console.log(arr[arr.length - 1]);
+
+//
+// let cars = ['bmw', 'honda','mers','lexus'];
+//  cars[1] = 'toyota';
+//
+//  console.log(cars);
+
+ // let cars = ['bmw', 'honda','mers','lexus'];
+ //
+ // cars.unshift('kia');
+ //
+ // console.log(cars)
+//
+//  let cars = ['bmw', 'honda','mers','lexus'];
+// cars.pop();
+//
+//  console.log(cars)
