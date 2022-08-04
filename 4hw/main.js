382053cf8343cb99fb68058244ede5f12cb74490
Nursultan24 <Nursultanmustafaev15@gmail.com>
2022-08-04 21:18:39 +0600

    second push 4hw

--- 4hw/main.js
+++ 4hw/main.js
@@ -19,14 +19,12 @@
 //
 // console.log(n);
 
-// let person = {
-//     age:18
-// };
+// let person = confirm('Тебе есть 18?');
 //
-// if (person.age >= 18){
-//     console.log('проходи')
+// if (person) {
+//     console.log('есть')
 // }else{
-//     console.log('не проходишь')
+//     console.log('нет')
 // }
 //
 // let a = 2;
@@ -78,6 +76,68 @@
 // }
 // if (time.a>45 && time.a<=60){
 //     console.log("время относится к целому часу")
+// }
+
+// let time = prompt('когда он собирается прийти в магазин? ');
+//
+// switch (time) {
+//     case  "23.00":
+//     case  "00.00":
+//     case  "01.00":
+//     case  "02.00":
+//     case  "03.00":
+//     case  "04.00":
+//     case  "05.00":
+//     case  "06.00":
+//     case  "07.00":
+//     case  "08.00":
+//     case  "09.00":
+//         alert('В это время мы не работаем');
+//         break;
+//     case  "10.00":
+//     case  "11.00":
+//         alert('Будем рады Вас видеть');
+//         break;
+//     case  "12.00":
+//     case  "13.00":
+//         alert('У нас обед');
+//         break;
+//     case  "14.00":
+//     case  "15.00":
+//     case  "16.00":
+//     case  "17.00":
+//     case  "18.00":
+//     case  "19.00":
+//     case  "20.00":
+//     case  "21.00":
+//     case  "22.00":
+//         alert('Будем рады Вас видеть')
+//         break
+// }
+
+// let seasons = "март";
+//
+// switch (seasons) {
+//     case "декабрь":
+//     case "январь":
+//     case "февраль":
+//         console.log("winter");
+//         break;
+//     case "март":
+//     case "апрель":
+//     case "май":
+//         console.log("Spring")
+//         break
+//     case "июнь":
+//     case "июль":
+//     case "август":
+//         console.log("Summer")
+//         break
+//     case "сентябрь":
+//     case "октябрь":
+//     case "ноябрь":
+//         console.log("Autumn")
+//         break
 // }
 
 
