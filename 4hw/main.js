d09ae745c79d5c042aa8b42bbcc7605344dc7791
Nursultan24 <Nursultanmustafaev15@gmail.com>
2022-08-04 18:52:04 +0600

    push 4hw

---
+++ 4hw/main.js
@@ -0,0 +1,85 @@
+// let name = 'test';
+//
+// if (name === 'tesssat') {
+//     console.log('Верно')
+// } else  {
+//     console.log('Неверно')
+// }
+
+//
+// let m = 10;
+// let n;
+//
+// if (m > 50) {
+//     n = 'большое'
+// }
+// else {
+//     n = 'маленькое'
+// }
+//
+// console.log(n);
+
+// let person = {
+//     age:18
+// };
+//
+// if (person.age >= 18){
+//     console.log('проходи')
+// }else{
+//     console.log('не проходишь')
+// }
+//
+// let a = 2;
+//
+// if (a === 0 ||  a ===2) {
+//     console.log(a + 7)
+// }
+// else {
+//     console.log(a / 10)
+// }
+//
+// let a = 1;
+//  b = 3;
+//
+// if (a <= 1 && b>=3){
+//     console.log(a*b)
+// }
+//
+// else {
+//     console.log(a/b)
+// }
+//  let number;
+//        data = true;
+//
+// number = (data === true) ? number = 2 :  number = 3;
+// console.log(number);
+
+// let Pasha = {
+//     age:18
+// };
+// if (Pasha.age <= 17) {
+//     console.log('ты ребенок')
+// }
+// else {
+//     console.log('ты взрослый')
+// }
+
+// let time ={
+//     a:47
+// };
+// if (time.a >= 0 && time.a <=15){
+//     console.log("время относится к четверти часа")
+// }
+// if (time.a >15 && time.a <=30){
+//     console.log("время относится к половине часа")
+// }
+// if (time.a >30 && time.a <= 45){
+//     console.log("время относится к 3/4 часа")
+// }
+// if (time.a>45 && time.a<=60){
+//     console.log("время относится к целому часу")
+// }
+
+
+
+
